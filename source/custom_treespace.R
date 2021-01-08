0811c4799c0feda8ba24446fb9d45cee20274585
maRce10 <marceloa27@gmail.com>
2021-01-07 20:43:32 -0600

    added rmds

--- source/custom_treespace.R
+++ source/custom_treespace.R
@@ -1,45 +1,46 @@
 # function to leave only most recent song types (from the last year in the data)
-# method = "last_year" for leaving only the tips found in the last year that a lek was sampled (for all fossil models) and "shared" to leave only those tips shared by all trees (a proportion given by "prop.trees")
-tree_prunner <- function(trees, method = "last_year", prop.trees = 0.9){
-
-  tl <- unique(unlist(lapply(trees,  function(x) x$tip.label)))
-
-  if (method == "last_year")
-  {
-  # years in tree tips
-  yrs <- as.numeric(substr(start = 8, 12, x = tl))
-
-  # most recent year
-  mryr <- max(yrs)
-
-  # target tips
-  trg.tps <- grep(x = tl, pattern =  mryr, value = TRUE)
-  }
-
-  if (method == "shared")
-  {
-    # tip count
-    tb <- table(unlist(lapply(trees,  function(x) x$tip.label)))
-
-    # those tips found in 90% of the trees
-    trg.tps <- names(tb[tb >= length(trees) * prop.trees])
-  }
-
-  prunned.trees <- lapply(trees, drop.tip, setdiff(tl, trg.tps))
-
-  ntips <- sapply(prunned.trees, Ntip)
-
-  prunned.trees <- prunned.trees[ntips == max(ntips)]
-
-  class(prunned.trees) <- "multiPhylo"
-
-  return(prunned.trees)
-}
+# method = "most_recent_year" for leaving only the tips found in the last year that a lek was sampled (for all fossil models) and "shared" to leave only those tips shared by all trees (a proportion given by "prop.trees")
+# tree_prunner <- function(trees, method = "most_recent_year", prop.trees = 0.9){
+#
+#   library(ape)
+#   library(phangorn)
+#
+#   tl <- unique(unlist(lapply(trees,  function(x) x$tip.label)))
+#
+#   if (method == "most_recent_year")
+#   {
+#   # years in tree tips
+#   yrs <- as.numeric(substr(start = 8, 12, x = tl))
+#
+#   # most recent year
+#   mryr <- max(yrs)
+#
+#   # target tips
+#   trg.tps <- grep(x = tl, pattern =  mryr, value = TRUE)
+#   }
+#
+#   if (method == "shared")
+#   {
+#     # tip count
+#     tb <- table(unlist(lapply(trees,  function(x) x$tip.label)))
+#
+#     # those tips found in 90% of the trees
+#     trg.tps <- names(tb[tb >= length(trees) * prop.trees])
+#   }
+#
+#   prunned.trees <- lapply(trees, drop.tip, setdiff(tl, trg.tps))
+#
+#   ntips <- sapply(prunned.trees, Ntip)
+#
+#   prunned.trees <- prunned.trees[ntips == max(ntips)]
+#
+#   class(prunned.trees) <- "multiPhylo"
+#
+#   return(prunned.trees)
+# }
 
 # multi_list = a named list of multiphylo objects, names are used for labeling chains
-custom_treespace <- function (multi_list, n.points = 100, thinning = 5000, method = "last_year"){
-
-  if (method == "last_year") {
+custom_treespace <- function (multi_list, n.points = 100, thinning = 5000, method = "RF"){
 
     # get subset of n.points for each list
     if (!is.infinite(n.points))
@@ -56,7 +57,6 @@ custom_treespace <- function (multi_list, n.points = 100, thinning = 5000, metho
 
       multi_list[[y]] <- multi_list[[y]][indices]
     }
-  }
 
   # add sample and model tag to each tree
     for(y in 1:length(multi_list)) {
@@ -70,35 +70,15 @@ custom_treespace <- function (multi_list, n.points = 100, thinning = 5000, metho
 
   # put trees in a single list
   alltrees <- unlist(multi_list, recursive = FALSE)
-
-  # prune trees
-  alltrees <- tree_prunner(trees = alltrees, method)
-
- if (method == "shared"){
-   # extract labels
-  model <- sapply(alltrees, "[[", "model")
-  mods <- split(names(model), model)
-
-  # get subset of n.points for each list
-  mods <- lapply(mods, function(x){
-    if (length(x) > n.points)
-    x <- x[round(seq(from = 1, to = length(x), length.out = n.points), 0)]
-    return(x)
-    })
-
-  mods <- unlist(mods)
-  alltrees <- alltrees[names(model) %in% mods]
-  }
-
   samples <- sapply(alltrees, "[[", "sample")
   model <- sapply(alltrees, "[[", "model")
 
   # get distance
-  d <- try(tree.dist.matrix(alltrees), silent = TRUE)
+  # d <- try(tree.dist.matrix(alltrees), silent = TRUE)
 
-  if (is(d, "try-error"))
-  d <- as.matrix(custom_mRF(trees = alltrees, normalize = FALSE, rooted = FALSE))
-
+  # if (is(d, "try-error"))
+  # d <- as.matrix(custom_mRF(trees = alltrees, normalize = FALSE, rooted = FALSE))
+    d <- as.matrix(topo_dist(trees = alltrees, method = method))
 
   if (sum(d) == 0) {
     x <- rep(0, length(alltrees))
@@ -118,80 +98,145 @@ custom_treespace <- function (multi_list, n.points = 100, thinning = 5000, metho
 }
 
 ###
-custom_mRF <- function(trees, normalize = FALSE, rooted = FALSE)
-{
-  if (!inherits(trees, "multiPhylo"))
-    stop("trees should be an object of class \"multiPhylo\"")
-  trees <- .compressTipLabel(trees)
-  tipLabel <- attr(trees, "TipLabel")
-  nTips <- length(tipLabel)
-  l <- length(trees)
-  RF <- numeric((l * (l - 1))/2)
-  if (rooted & any(!is.rooted(trees))) {
-    warning("some trees were rooted, unrooted all")
-    rooted <- FALSE
+# custom_mRF <- function(trees, normalize = FALSE, rooted = FALSE)
+# {
+#   if (!inherits(trees, "multiPhylo"))
+#     stop("trees should be an object of class \"multiPhylo\"")
+#   trees <- .compressTipLabel(trees)
+#   tipLabel <- attr(trees, "TipLabel")
+#   nTips <- length(tipLabel)
+#   l <- length(trees)
+#   RF <- numeric((l * (l - 1))/2)
+#   if (rooted & any(!is.rooted(trees))) {
+#     warning("some trees were rooted, unrooted all")
+#     rooted <- FALSE
+#   }
+#   if (!rooted) {
+#     if (any(is.rooted(trees))) {
+#       trees <- unroot(trees)
+#     }
+#   }
+#   if (any(has.singles(trees)))
+#     trees <- lapply(trees, collapse.singles)
+#   # if (any(!is.binary(trees))) {
+#   #   message("Some trees are not binary. Result may not what you expect!")
+#   # }
+#   Nnodes <- sapply(trees, Nnode)
+#   trees <- .uncompressTipLabel(trees)
+#   trees <- unclass(trees)
+#   xx <- lapply(trees, bipart)
+#   if (!rooted)
+#     xx <- lapply(xx, SHORTwise, nTips)
+#   xx <- lapply(xx, function(x) sapply(x, paste, collapse = "_"))
+#   k <- 1
+#   for (i in 1:(l - 1)) {
+#     tmp <- xx[[i]]
+#     for (j in (i + 1):l) {
+#       RF[k] <- Nnodes[i] + Nnodes[j] - 2 * sum(fastmatch::fmatch(xx[[j]],
+#                                                       tmp, nomatch = 0L) > 0L)
+#       if (normalize)
+#         RF[k] <- RF[k]/(Nnodes[i] + Nnodes[j] - 2)
+#       k <- k + 1
+#     }
+#   }
+#   attr(RF, "Size") <- l
+#   if (!is.null(names(trees)))
+#     attr(RF, "Labels") <- names(trees)
+#   attr(RF, "Diag") <- FALSE
+#   attr(RF, "Upper") <- FALSE
+#   class(RF) <- "dist"
+#   return(RF)
+# }
+#
+# SHORTwise <- function(x, nTips, delete = FALSE) {
+#   v <- 1:nTips
+#   l <- lengths(x)
+#   lv <- floor(nTips / 2)
+#   for (i in seq_along(x)) {
+#     if (l[i] > lv) {
+#       y <- x[[i]]
+#       x[[i]] <- v[-y]
+#     }
+#     if (l[i] == nTips / 2) {
+#       y <- x[[i]]
+#       if (y[1] != 1)
+#         x[[i]] <- v[-y]
+#     }
+#   }
+#   if (any(l == nTips) && delete) {
+#     x <- x[l != nTips]
+#   }
+#   x
+# }
+#
+# bipart <- function(x) {
+#   x <- reorder(x, "postorder")
+#   nTips <- as.integer(length(x$tip.label))
+#   .Call("_phangorn_bipartCPP", PACKAGE = "phangorn", x$edge, nTips)
+# }
+
+# methods are: "RF", "wRF", "KF", "path"
+# methods as in phangorn::treedist function names
+
+
+compare_trees <- function(t1, t2, method){
+
+  # drop unshared tips
+  if (length(c(setdiff(t1$tip.label, t2$tip.label), setdiff(t2$tip.label, t1$tip.label))) > 0){
+
+    common_tips <- intersect(t1$tip.label, t2$tip.label)
+
+    t1 <- drop.tip(t1, setdiff(t1$tip.label, common_tips))
+
+    t2 <- drop.tip(t2, setdiff(t2$tip.label, common_tips))
   }
-  if (!rooted) {
-    if (any(is.rooted(trees))) {
-      trees <- unroot(trees)
-    }
-  }
-  if (any(has.singles(trees)))
-    trees <- lapply(trees, collapse.singles)
-  # if (any(!is.binary(trees))) {
-  #   message("Some trees are not binary. Result may not what you expect!")
-  # }
-  Nnodes <- sapply(trees, Nnode)
-  trees <- .uncompressTipLabel(trees)
-  trees <- unclass(trees)
-  xx <- lapply(trees, bipart)
-  if (!rooted)
-    xx <- lapply(xx, SHORTwise, nTips)
-  xx <- lapply(xx, function(x) sapply(x, paste, collapse = "_"))
-  k <- 1
-  for (i in 1:(l - 1)) {
-    tmp <- xx[[i]]
-    for (j in (i + 1):l) {
-      RF[k] <- Nnodes[i] + Nnodes[j] - 2 * sum(fastmatch::fmatch(xx[[j]],
-                                                      tmp, nomatch = 0L) > 0L)
-      if (normalize)
-        RF[k] <- RF[k]/(Nnodes[i] + Nnodes[j] - 2)
-      k <- k + 1
-    }
-  }
-  attr(RF, "Size") <- l
-  if (!is.null(names(trees)))
-    attr(RF, "Labels") <- names(trees)
-  attr(RF, "Diag") <- FALSE
-  attr(RF, "Upper") <- FALSE
-  class(RF) <- "dist"
-  return(RF)
+
+  # run if both trees have at least 3 tips
+  if (all(c(Ntip(t1), Ntip(t2)) > 2)){
+  if (method == "RF")
+    d <- phangorn::RF.dist(ape::unroot(t1), ape::unroot(t2))
+
+  if (method == "wRF")
+    d <- phangorn::wRF.dist(ape::unroot(t1), ape::unroot(t2))
+
+  if (method == "KF")
+    d <- phangorn::KF.dist(ape::unroot(t1), ape::unroot(t2))
+
+  if (method == "path")
+    d <- phangorn::path.dist(ape::unroot(t1), ape::unroot(t2))
+  } else d <- NA
+  return(d)
 }
 
-SHORTwise <- function(x, nTips, delete = FALSE) {
-  v <- 1:nTips
-  l <- lengths(x)
-  lv <- floor(nTips / 2)
-  for (i in seq_along(x)) {
-    if (l[i] > lv) {
-      y <- x[[i]]
-      x[[i]] <- v[-y]
-    }
-    if (l[i] == nTips / 2) {
-      y <- x[[i]]
-      if (y[1] != 1)
-        x[[i]] <- v[-y]
-    }
-  }
-  if (any(l == nTips) && delete) {
-    x <- x[l != nTips]
-  }
-  x
-}
 
-bipart <- function(x) {
-  x <- reorder(x, "postorder")
-  nTips <- as.integer(length(x$tip.label))
-  .Call("_phangorn_bipartCPP", PACKAGE = "phangorn", x$edge, nTips)
-}
+topo_dist <- function(trees, method = "RF"){
 
+  tree_names <- names(trees)
+
+  combs <- t(combn(tree_names, 2))
+
+  dsts <- sapply(1:nrow(combs), function(x){
+
+    # extract 2 trees
+    t1 <- trees[[combs[x, 1]]]
+    t2 <- trees[[combs[x, 2]]]
+
+    d <- try(compare_trees(t1, t2, method))
+
+    if (is(d, "try-error")) return(NA) else return(d)
+  })
+
+  # put results in a distance matrix
+  mat <- matrix(nrow = length(tree_names), ncol = length(tree_names))
+  mat[] <- 0
+  colnames(mat) <- rownames(mat) <- tree_names
+  mat[lower.tri(mat, diag = FALSE)] <- dsts
+  mat <- t(mat)
+  mat[lower.tri(mat, diag = FALSE)] <- dsts
+
+  # impute missing values
+  if (anyNA(mat))
+    mat <- ape::additive(as.dist(mat))
+
+  return(mat)
+}
